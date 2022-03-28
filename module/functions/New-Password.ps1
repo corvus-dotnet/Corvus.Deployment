d33e33dae9d18a77258d2aea1c21dcda01265b8f
James Dawson <james.dawson@endjin.com>
2022-03-28 17:39:21 +0100

    Added 'New-Password.ps1'

--- module/functions/New-Password.ps1
+++ module/functions/New-Password.ps1
@@ -15,6 +15,12 @@ The length of the password to be generated.
 .PARAMETER ValidSymbols
 A character array containing the characters that will be treated as symbols when validating the password strength.
 
+.PARAMETER KeyVaultName
+The optional key vault where the password will be stored.
+
+.PARAMETER KeyVaultSecretName
+The key vault secret name where the password will be stored.
+
 .OUTPUTS
 SecureString
 
@@ -26,14 +32,25 @@ $pwd = New-Password -Length 18
 #>
 function New-Password
 {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName="Default")]
     param
     (
+        [Parameter(ParameterSetName="Default")]
         [ValidateRange(12, 256)]
         [int] $Length = 16,
 
+        [Parameter(ParameterSetName="Default")]
         [ValidateNotNull()]
-        [char[]] $ValidSymbols = '!@#$%^&*'.ToCharArray()
+        [char[]] $ValidSymbols = '!@#$%^&*'.ToCharArray(),
+
+        [Parameter(ParameterSetName="Default")]
+        [switch] $PassThru,
+
+        [Parameter(Mandatory=$true, ParameterSetName="UseKeyVault")]
+        [string] $KeyVaultName,
+        
+        [Parameter(Mandatory=$true, ParameterSetName="UseKeyVault")]
+        [string] $KeyVaultSecretName
     )
 
     # reference: https://gist.github.com/onlyann/00d9bb09d4b1338ffc88a213509a6caf
@@ -57,5 +74,19 @@ function New-Password
     until (($hasLowerChar + $hasUpperChar + $hasDigit + $hasSymbol) -eq 4)
     
     Write-Verbose "Password generated after $iterations iterations"
-    $password | ConvertTo-SecureString -AsPlainText
+    $securePassword = $password | ConvertTo-SecureString -AsPlainText
+    $password = $null
+
+    if ($PSCmdlet.ParameterSetName -eq "UseKeyVault") {
+        Write-Host "Storing password in key vault [VaultName=$KeyVaultName, SecretName=$KeyVaultSecretName]"
+        Set-AzKeyVaultSecret -VaultName $KeyVaultName `
+                                -Name $KeyVaultSecretName `
+                                -SecretValue $securePassword `
+                                -ContentType "text/plain" `
+            | Out-Null
+    }
+
+    if ($PassThru) {
+        $securePassword
+    }
 }
