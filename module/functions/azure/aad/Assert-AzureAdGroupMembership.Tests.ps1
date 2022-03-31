cc7b6080a18d7c662a1656040760aafac1bc1503
James Dawson <james.dawson@endjin.com>
2022-03-31 18:08:23 +0100

    Added 'Assert-AzureAdGroupMembership'

---
+++ module/functions/azure/aad/Assert-AzureAdGroupMembership.Tests.ps1
@@ -0,0 +1,216 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.ps1", ".ps1")
+
+. "$here\$sut"
+
+# Import other module dependendencies
+# . "$here\..\..\Select-ExceptIn.ps1"
+
+Describe "Assert-AzureAdGroupMembership Tests" {
+
+    $mockDuplicateGroups = @(
+        @{Id="00000000-0000-0000-0000-000000000000"; DisplayName="fakeGroup1"; SecurityEnabled=$true}
+        @{Id="11111111-1111-1111-1111-111111111111"; DisplayName="fakeGroup2"; SecurityEnabled=$true}
+    )
+
+    $mockGroup = @{
+        Id = "7c408c06-b467-4fd5-96e2-bc9cbc1bd4ee"
+        DisplayName = "some-group"
+        SecurityEnabled = $true
+    }
+    $mockGroupMembers = @(
+        @{Id = "6cea30de-a493-42b7-9855-2d9a343eca8f"}
+        @{Id = "9871e647-e493-4596-84d7-8bbbe8e90447"}
+    )
+
+    Context "Group does not exist" {
+
+        Mock Get-AzADGroup {}
+        Mock Get-AzureAdDirectoryObject {}
+
+        It "should throw an exception" {
+            $mockGroupName = "nonexistent-group"
+            { Assert-AzureAdGroupMembership -Name $mockGroupName -RequiredMembers @("user@nowwhere.org") } |
+                Should -Throw "The specified group could not be found: DisplayName=$mockGroupName"
+
+            Assert-MockCalled Get-AzureAdDirectoryObject -Times 0
+        }
+    }
+
+    Context "Multiple groups found" {
+
+        Mock Get-AzADGroup { $mockDuplicateGroups }
+        Mock Get-AzureAdDirectoryObject {}
+
+        It "should throw an exception" {
+            { Assert-AzureAdGroupMembership -Name "a-common-group-name" -RequiredMembers @("user@nowwhere.org") } | 
+                Should -Throw "Found multiple matching groups: ObjectId=$($mockDuplicateGroups[0].Id); ObjectId=$($mockDuplicateGroups[1].Id);"
+
+            Assert-MockCalled Get-AzureAdDirectoryObject -Times 0
+        }
+    }
+
+    Context "Required members are already in the group (one member)" {
+
+        Mock Get-AzADGroup { $mockGroup }
+        Mock Get-AzADGroupMember { $mockGroupMembers[0] }
+        Mock Get-AzureAdDirectoryObject { $mockGroupMembers[0] }
+        Mock Add-AzADGroupMember {}
+        Mock Remove-AzADGroupMember {}
+
+        Assert-AzureAdGroupMembership `
+            -Name $mockGroup.DisplayName `
+            -RequiredMembers @($mockGroupMembers[0].Id)
+
+        It "should not try to update the group" {
+            Assert-MockCalled Get-AzureAdDirectoryObject -Times 1
+            Assert-MockCalled Add-AzADGroupMember -Times 0
+            Assert-MockCalled Remove-AzADGroupMember -Times 0
+        }
+    }
+
+    Context "Required members are already in the group (multiple members)" {
+
+        Mock Get-AzADGroup { $mockGroup }
+        Mock Get-AzADGroupMember { $mockGroupMembers }
+        Mock Get-AzureAdDirectoryObject { $mockGroupMembers[0] } -ParameterFilter { $Criterion -eq $mockGroupMembers[0].Id }
+        Mock Get-AzureAdDirectoryObject { $mockGroupMembers[1] } -ParameterFilter { $Criterion -eq $mockGroupMembers[1].Id }
+        Mock Add-AzADGroupMember {}
+        Mock Remove-AzADGroupMember {}
+
+        Assert-AzureAdGroupMembership `
+            -Name $mockGroup.DisplayName `
+            -RequiredMembers ($mockGroupMembers | Select-Object -ExpandProperty Id)
+
+        It "should not try to update the group" {
+            Assert-MockCalled Get-AzureAdDirectoryObject -Times 2
+            Assert-MockCalled Add-AzADGroupMember -Times 0
+            Assert-MockCalled Remove-AzADGroupMember -Times 0
+        }
+    }
+
+    Context "Missing single member" {
+
+        Mock Get-AzADGroup { $mockGroup }
+        Mock Get-AzADGroupMember { $mockGroupMembers[0] }
+        Mock Get-AzureAdDirectoryObject { $mockGroupMembers[0] } -ParameterFilter { $Criterion -eq $mockGroupMembers[0].Id }
+        Mock Get-AzureAdDirectoryObject { $mockGroupMembers[1] } -ParameterFilter { $Criterion -eq $mockGroupMembers[1].Id }
+        Mock Add-AzADGroupMember {}
+        Mock Remove-AzADGroupMember {}
+
+        Assert-AzureAdGroupMembership `
+            -Name $mockGroup.DisplayName `
+            -RequiredMembers ($mockGroupMembers | Select-Object -ExpandProperty Id)
+
+        It "should add the missing member to the group" {
+            Assert-MockCalled Get-AzureAdDirectoryObject -Times 2
+            Assert-MockCalled Add-AzADGroupMember -Times 1 -ParameterFilter { $MemberObjectId -eq $mockGroupMembers[1].Id }
+            Assert-MockCalled Remove-AzADGroupMember -Times 0
+        }
+    }
+
+    Context "Missing multiple members" {
+
+        Mock Get-AzADGroup { $mockGroup }
+        Mock Get-AzADGroupMember { @() }
+        Mock Get-AzureAdDirectoryObject { $mockGroupMembers[0] } -ParameterFilter { $Criterion -eq $mockGroupMembers[0].Id }
+        Mock Get-AzureAdDirectoryObject { $mockGroupMembers[1] } -ParameterFilter { $Criterion -eq $mockGroupMembers[1].Id }
+        Mock Add-AzADGroupMember {}
+        Mock Remove-AzADGroupMember {}
+
+        Assert-AzureAdGroupMembership `
+            -Name $mockGroup.DisplayName `
+            -RequiredMembers ($mockGroupMembers | Select-Object -ExpandProperty Id)
+
+        It "should add the missing member to the group" {
+            Assert-MockCalled Get-AzureAdDirectoryObject -Times 2
+            Assert-MockCalled Add-AzADGroupMember -Times 1 -ParameterFilter { $MemberObjectId -eq $mockGroupMembers[0].Id }
+            Assert-MockCalled Add-AzADGroupMember -Times 1 -ParameterFilter { $MemberObjectId -eq $mockGroupMembers[1].Id }
+            Assert-MockCalled Remove-AzADGroupMember -Times 0
+        }
+    }
+
+    Context "Additional members are already in the group (Non-Strict)" {
+
+        Mock Get-AzADGroup { $mockGroup }
+        Mock Get-AzADGroupMember { $mockGroupMembers }
+        Mock Get-AzureAdDirectoryObject { $mockGroupMembers[0] }
+        Mock Add-AzADGroupMember {}
+        Mock Remove-AzADGroupMember {}
+
+        Assert-AzureAdGroupMembership `
+            -Name $mockGroup.DisplayName `
+            -RequiredMembers @($mockGroupMembers[0].Id)
+
+        It "should not try to update the group" {
+            Assert-MockCalled Add-AzADGroupMember -Times 0
+            Assert-MockCalled Remove-AzADGroupMember -Times 0
+        }
+    }
+
+    Context "Additional members are already in the group (Strict)" {
+
+        Mock Get-AzADGroup { $mockGroup }
+        Mock Get-AzADGroupMember { $mockGroupMembers }
+        Mock Get-AzureAdDirectoryObject { $mockGroupMembers[0] }
+        Mock Add-AzADGroupMember {}
+        Mock Remove-AzADGroupMember {}
+
+        Assert-AzureAdGroupMembership `
+            -Name "nonexistent-group" `
+            -RequiredMembers @($mockGroupMembers[0].Id) `
+            -StrictMode $true
+
+        It "should remove extraneous members" {
+            Assert-MockCalled Add-AzADGroupMember -Times 0
+            Assert-MockCalled Remove-AzADGroupMember -Times 1 -ParameterFilter { $MemberObjectId -eq $mockGroupMembers[1].Id }
+        }
+    }
+
+    Context "Adding and removing group members (Strict)" {
+
+        $mockExistingMembers = $mockGroupMembers + @{Id = ([guid]::Empty).Guid.ToString()}
+        $requiredMembers = $mockGroupMembers
+
+        Mock Get-AzADGroup { $mockGroup }
+        Mock Get-AzADGroupMember { @($mockExistingMembers[0], $mockExistingMembers[2]) }
+        Mock Get-AzureAdDirectoryObject { $mockExistingMembers[0] } -ParameterFilter { $Criterion -eq $mockExistingMembers[0].Id }
+        Mock Get-AzureAdDirectoryObject { $mockExistingMembers[1] } -ParameterFilter { $Criterion -eq $mockExistingMembers[1].Id }
+        Mock Get-AzureAdDirectoryObject { $mockExistingMembers[2] } -ParameterFilter { $Criterion -eq $mockExistingMembers[2].Id }
+        # We need to return a group this time, so we still have a group object for the subsequent call Remove-AzADGroupMember
+        Mock Add-AzADGroupMember { $mockGroup }
+        Mock Remove-AzADGroupMember {}
+
+        Assert-AzureAdGroupMembership `
+            -Name $mockGroup.DisplayName `
+            -RequiredMembers ($requiredMembers | Select-Object -ExpandProperty Id) `
+            -StrictMode $true
+
+        It "should remove extraneous members" {
+            Assert-MockCalled Get-AzureAdDirectoryObject -Times 1 -ParameterFilter { $Criterion -eq $mockExistingMembers[0].Id }
+            Assert-MockCalled Get-AzureAdDirectoryObject -Times 1 -ParameterFilter { $Criterion -eq $mockExistingMembers[1].Id }
+            Assert-MockCalled Add-AzADGroupMember -Times 1 -ParameterFilter { $MemberObjectId -eq $mockExistingMembers[1].Id }
+            Assert-MockCalled Remove-AzADGroupMember -Times 1 -ParameterFilter { $MemberObjectId -eq $mockExistingMembers[2].Id }
+        }
+    }
+
+    Context "Adding an invalid member" {
+        Mock Get-AzADGroup { $mockGroup }
+        Mock Get-AzADGroupMember { @() }
+        Mock Get-AzureAdDirectoryObject {} -ParameterFilter { $Criterion -eq [guid]::Empty }
+        Mock Add-AzADGroupMember {}
+        Mock Remove-AzADGroupMember {}
+        Mock Write-Warning {}
+
+        Assert-AzureAdGroupMembership `
+            -Name $mockGroup.DisplayName `
+            -RequiredMembers @([guid]::Empty)
+
+        It "should not add the member, but log a warning instead" {
+            Assert-MockCalled Get-AzureAdDirectoryObject -Times 1
+            Assert-MockCalled Add-AzADGroupMember -Times 0
+            Assert-MockCalled Remove-AzADGroupMember -Times 0
+            Assert-MockCalled Write-Warning -Times 1
+        }
+    }
+}
---
+++ module/functions/azure/aad/Assert-AzureAdGroupMembership.ps1
@@ -0,0 +1,115 @@
+# <copyright file="Assert-AzureAdGroupMembership.ps1" company="Endjin Limited">
+# Copyright (c) Endjin Limited. All rights reserved.
+# </copyright>
+
+<#
+.SYNOPSIS
+Configures the membership of an AzureAD group.
+
+.DESCRIPTION
+Uses Azure PowerShell to manage AzureAD group membership.
+
+.PARAMETER Name
+The display name of the group.
+
+.PARAMETER ObjectId
+The objectId of the group.
+
+.PARAMETER Members
+The list of AzureAD objects that should be members of the group. These can be specified using
+'DisplayName', 'ObjectId', 'ApplicationId' or 'UserPrincipalName'.
+
+.PARAMETER StrictMode
+When true, existing group members not specified in the 'RequiredMembers' parameters will be removed from the group.
+
+.OUTPUTS
+AzureAD group definition object
+
+.EXAMPLE
+
+Assert-AzureAdGroupMembership -Name "MyGroup" -RequiredMembers @("MyOtherGroup", "MyUser", "MyServicePrincipal")
+Assert-AzureAdGroupMembership -Name "MyGroup" -RequiredMembers @("MyOtherGroup", "MyUser@nowhere.org", "be2a6313-cb3a-45ad-a70f-cbac2a8c565f")
+Assert-AzureAdGroupMembership -Name "MyGroup" -RequiredMembers @("f7f0545c-82b5-4008-bebf-f73fb1d5a7f8", "MyUser@nowhere.org", "be2a6313-cb3a-45ad-a70f-cbac2a8c565f")
+
+#>
+function Assert-AzureAdGroupMembership
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, ParameterSetName="ByName")]
+        [string] $Name,
+
+        [Parameter(Mandatory=$true, ParameterSetName="ByObjectId")]
+        [string] $ObjectId,
+
+        [Parameter()]
+        [string[]] $RequiredMembers,
+
+        [Parameter()]
+        [ValidateSet("Security","Distribution")]
+        [string] $GroupType = "Security",
+
+        [Parameter()]
+        [bool] $StrictMode
+    )
+
+    # Setup the parameters for 'Get-AzADGroup' based on whether we've been given a group Name or ObjectId
+    $groupLookupSplat = $PSCmdlet.ParameterSetName -eq "ByName" ? @{DisplayName = $Name} : @{ObjectId = $ObjectId}
+    [array]$group = Get-AzADGroup @groupLookupSplat | `
+        Where-Object { $_.SecurityEnabled -eq ($GroupType -eq "Security") }
+
+    if (!$group) {
+        throw "The specified group could not be found: $($groupLookupSplat.Keys[0])=$($groupLookupSplat.Values[0])"
+    }
+    elseif ($group.Count -gt 1) {
+        throw "Found multiple matching groups: $($group | % { "ObjectId=$($_.Id);"} )"
+    }
+
+    Write-Information "Processing group membership for '$($group.DisplayName)' [ObjectId=$($group.Id)]"
+
+    $existingMemberObjectIds = $group | Get-AzADGroupMember | Select-Object -ExpandProperty Id
+
+    # Required members can be specified in various forms:
+    #   - ObjectId (groups, users & service principals)
+    #   - DisplayName (groups, users & service principals)
+    #   - UserPrincipalName (users only)
+    #   - ApplicationId (service principals only)
+    #
+    # We need the ObjectId to add them to the group - this block handles
+    # resolving the above forms into an ObjectId
+    $requiredMemberObjectIds = @()
+    foreach ($member in $RequiredMembers) {   
+        # At this stage we don't know whether the required member is a user, group or service principal.
+        # This helper will handle that lookup.
+        $resolvedMember = Get-AzureAdDirectoryObject -Criterion $member -Single
+        if ($resolvedMember) {
+            $requiredMemberObjectIds += $resolvedMember.Id
+        }
+        else {
+            Write-Warning "Skipping '$member' - not found in the directory"
+        }
+    }
+  
+    # Add any missing required members
+    $membersToAdd = $requiredMemberObjectIds | Where-Object { $_ -notin $existingMemberObjectIds }
+    if ($membersToAdd) {
+        Write-Information "Adding members: $($membersToAdd -join ', ')"
+        $group = $group | Add-AzADGroupMember -MemberObjectId $membersToAdd -PassThru
+    }
+    else {
+        Write-Information "No members to add"
+    }
+
+    # Remove extraneous members, only when StrictMode is enabled
+    if ($StrictMode) {
+        $membersToRemove = $existingMemberObjectIds | Where-Object { $_ -notin $requiredMemberObjectIds }
+        if ($membersToRemove) {
+            Write-Information "Removing members: $($membersToRemove -join ', ')"
+            $group = $group | Remove-AzADGroupMember -MemberObjectId $membersToRemove -PassThru
+        }
+        else {
+            Write-Information "No members to remove"
+        }
+    }
+
+}
