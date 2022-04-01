3c11af5bec16a3dcbd71f054e6f881840bb5db75
James Dawson <james.dawson@endjin.com>
2022-04-01 11:43:07 +0100

    Improved tests

--- module/functions/azure/aad/Assert-AzureAdSecurityGroup.Tests.ps1
+++ module/functions/azure/aad/Assert-AzureAdSecurityGroup.Tests.ps1
@@ -5,8 +5,9 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.ps1", ".p
 
 # define other functions that will be mocked
 function _EnsureAzureConnection {}
+function Get-AzureAdDirectoryObject { param($Criterion) }
 function Get-AzADGroup {}
-function Invoke-AzRestMethod {}
+function Invoke-AzRestMethod { param($Uri,$Payload) }
 
 Describe "Assert-AzureAdSecurityGroup Tests" {
 
@@ -20,7 +21,7 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
         Mock _getGroupOwners {}
         Mock Invoke-AzRestMethod {}
 
-        Context "No default owners" {
+        Context "No default owners specified" {
             $testGroup = @{
                 Name = 'testgroup'
                 EmailName = 'testgroup@nowhere.org'
@@ -28,16 +29,19 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
                 OwnersToAssignOnCreation = @()
                 StrictMode = $false
             }
+            Mock Get-AzureAdDirectoryObject {}
+
             Assert-AzureAdSecurityGroup @testGroup
 
             It "should create the group" {
                 Assert-MockCalled _buildUpdateRequest -Times 0
+                Assert-MockCalled Get-AzureAdDirectoryObject -Times 0
                 Assert-MockCalled _getGroupOwners -Times 0
-                Assert-MockCalled Invoke-AzRestMethod -Times 1
+                Assert-MockCalled Invoke-AzRestMethod -Times 1 -ParameterFilter { $Uri -eq "https://graph.microsoft.com/v1.0/groups" -and $Payload -notmatch "owners" }
             }
         }
 
-        Context "Default owners" {
+        Context "Default owner specified" {
             $testGroup = @{
                 Name = 'testgroup'
                 EmailName = 'testgroup@nowhere.org'
@@ -45,21 +49,54 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
                 OwnersToAssignOnCreation = @("someone@nowhere.org")
                 StrictMode = $false
             }
+            $mockOwnerObjectId = [guid]::NewGuid().ToString()
+            Mock Get-AzureAdDirectoryObject { $mockOwnerObjectId }
+
             Assert-AzureAdSecurityGroup @testGroup
 
-            It "should create the group" {
+            It "should create the group with the specified owner" {
                 Assert-MockCalled _buildUpdateRequest -Times 0
+                Assert-MockCalled Get-AzureAdDirectoryObject -Times 1
                 Assert-MockCalled _getGroupOwners -Times 0
-                Assert-MockCalled Invoke-AzRestMethod -Times 1
+                Assert-MockCalled Invoke-AzRestMethod -Times 1 -ParameterFilter { $Uri -eq "https://graph.microsoft.com/v1.0/groups" -and $Payload -match $mockOwnerObjectId }
+            }
+        }
+
+        Context "Multiple default owners specified" {
+            $testGroup = @{
+                Name = 'testgroup'
+                EmailName = 'testgroup@nowhere.org'
+                Description = 'just a test group'
+                OwnersToAssignOnCreation = @("someone@nowhere.org","MyServicePrincipal")
+                StrictMode = $false
+            }
+            $mockOwnerObjectIds = @( [guid]::NewGuid().ToString(), [guid]::NewGuid().ToString() )
+            
+            Mock Get-AzureAdDirectoryObject { $mockOwnerObjectIds[0] } -ParameterFilter { $Criterion -eq "someone@nowhere.org" }
+            Mock Get-AzureAdDirectoryObject { $mockOwnerObjectIds[1] } -ParameterFilter { $Criterion -eq "MyServicePrincipal" }
+
+            Assert-AzureAdSecurityGroup @testGroup
+
+            It "should create the group specifying all the required owners" {
+                Assert-MockCalled _buildUpdateRequest -Times 0
+                Assert-MockCalled Get-AzureAdDirectoryObject -Times 2
+                Assert-MockCalled _getGroupOwners -Times 0
+                Assert-MockCalled Invoke-AzRestMethod -Times 1 `
+                    -ParameterFilter { `
+                        $Uri -eq "https://graph.microsoft.com/v1.0/groups" -and `
+                        $Payload -match $mockOwnerObjectIds[0] -and `
+                        $Payload -match $mockOwnerObjectIds[1]
+                    }
             }
         }
     }
 
     Context "Group already exists" {
 
+        $groupObjectId = '00000000-0000-0000-0000-000000000000'
         Mock Get-AzADGroup { return @{
                 displayName = 'testgroup'
-                id = '00000000-0000-0000-0000-000000000000'
+                id = $groupObjectId
                 mailNickname = 'testgroup'
                 mailEnabled = $false
                 securityEnabled = $true
@@ -69,6 +106,8 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
         Mock _buildCreateRequest {}
         Mock _getGroupOwners { @('11111111-1111-1111-1111-111111111111') }
         Mock Invoke-AzRestMethod {}
+        $mockOwnerObjectId = [guid]::NewGuid().ToString()
+        Mock Get-AzureAdDirectoryObject { $mockOwnerObjectId }
         Mock Write-Warning {}
 
         Context "No default owners - No changes required" {
@@ -84,15 +123,17 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
             It "should not update the group" {
                 Assert-MockCalled _buildCreateRequest -Times 0
                 Assert-MockCalled _getGroupOwners -Times 0
+                Assert-MockCalled Get-AzureAdDirectoryObject -Times 0
                 Assert-MockCalled Invoke-AzRestMethod -Times 0
             }
         }
 
+        $updatedGroupDesc = 'just a test group with a different description'
         Context "Description updated (StrictMode=false)" {
             $testGroup = @{
                 Name = 'testgroup'
                 EmailName = 'testgroup@nowhere.org'
-                Description = 'just a test group with a different description'
+                Description = $updatedGroupDesc
                 OwnersToAssignOnCreation = @()
                 StrictMode = $false
             }
@@ -101,6 +142,7 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
             It "should not update the group" {
                 Assert-MockCalled _buildCreateRequest -Times 0
                 Assert-MockCalled _getGroupOwners -Times 0
+                Assert-MockCalled Get-AzureAdDirectoryObject -Times 0
                 Assert-MockCalled Invoke-AzRestMethod -Times 0
             }
         }
@@ -109,7 +151,7 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
             $testGroup = @{
                 Name = 'testgroup'
                 EmailName = 'testgroup@nowhere.org'
-                Description = 'just a test group with a different description'
+                Description = $updatedGroupDesc
                 OwnersToAssignOnCreation = @()
                 StrictMode = $true
             }
@@ -118,7 +160,12 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
             It "should update the group" {
                 Assert-MockCalled _buildCreateRequest -Times 0
                 Assert-MockCalled _getGroupOwners -Times 0
-                Assert-MockCalled Invoke-AzRestMethod -Times 1
+                Assert-MockCalled Get-AzureAdDirectoryObject -Times 0
+                Assert-MockCalled Invoke-AzRestMethod -Times 1 `
+                    -ParameterFilter {  
+                        $Uri -eq "https://graph.microsoft.com/v1.0/groups/$groupObjectId" -and  
+                        $Payload -notmatch "owners" -and $Payload -match $updatedGroupDesc
+                    }
             }
         }
 
@@ -127,7 +174,7 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
                 Name = 'testgroup'
                 EmailName = 'testgroup@nowhere.org'
                 Description = 'just a test group'
-                OwnersToAssignOnCreation = @('11111111-1111-1111-1111-111111111111')
+                OwnersToAssignOnCreation = @('anothergroup@nowhere.com')
                 StrictMode = $false
             }
             Assert-AzureAdSecurityGroup @testGroup
@@ -135,6 +182,7 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
             It "should not update the group" {
                 Assert-MockCalled _buildCreateRequest -Times 0
                 Assert-MockCalled _getGroupOwners -Times 0
+                Assert-MockCalled Get-AzureAdDirectoryObject -Times 1
                 Assert-MockCalled Write-Warning -Times 0
                 Assert-MockCalled Invoke-AzRestMethod -Times 0
             }
@@ -145,7 +193,7 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
                 Name = 'testgroup'
                 EmailName = 'testgroup@nowhere.org'
                 Description = 'just a test group'
-                OwnersToAssignOnCreation = @('22222222-2222-2222-2222-222222222222')
+                OwnersToAssignOnCreation = @('someone@nowhere.org')
                 StrictMode = $false
             }
             Assert-AzureAdSecurityGroup @testGroup
@@ -153,6 +201,7 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
             It "should not update the group" {
                 Assert-MockCalled _buildCreateRequest -Times 0
                 Assert-MockCalled _getGroupOwners -Times 0
+                Assert-MockCalled Get-AzureAdDirectoryObject -Times 1
                 Assert-MockCalled Write-Warning -Times 0
                 Assert-MockCalled Invoke-AzRestMethod -Times 0
             }
@@ -163,7 +212,7 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
                 Name = 'testgroup'
                 EmailName = 'testgroup@nowhere.org'
                 Description = 'just a test group'
-                OwnersToAssignOnCreation = @('22222222-2222-2222-2222-222222222222')
+                OwnersToAssignOnCreation = @('someone@nowhere.org')
                 StrictMode = $true
             }
             Assert-AzureAdSecurityGroup @testGroup
@@ -171,6 +220,7 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
             It "should not update the group and warn the owners cannot be updated" {
                 Assert-MockCalled _buildCreateRequest -Times 0
                 Assert-MockCalled _getGroupOwners -Times 1
+                Assert-MockCalled Get-AzureAdDirectoryObject -Times 1
                 Assert-MockCalled Write-Warning -Times 1
                 Assert-MockCalled Invoke-AzRestMethod -Times 0
             }
@@ -181,7 +231,7 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
                 Name = 'testgroup'
                 EmailName = 'testgroup@nowhere.org'
                 Description = 'just a test group'
-                OwnersToAssignOnCreation = @('22222222-2222-2222-2222-222222222222')
+                OwnersToAssignOnCreation = @('MyServicePrincipal')
                 # Omit the StrictMode parameter to simulate a consumer of an earlier version, before it was added
             }
             Assert-AzureAdSecurityGroup @testGroup
@@ -189,6 +239,7 @@ Describe "Assert-AzureAdSecurityGroup Tests" {
             It "should not update the group and warn the owners cannot be updated" {
                 Assert-MockCalled _buildCreateRequest -Times 0
                 Assert-MockCalled _getGroupOwners -Times 1
+                Assert-MockCalled Get-AzureAdDirectoryObject -Times 1
                 Assert-MockCalled Write-Warning -Times 1
                 Assert-MockCalled Invoke-AzRestMethod -Times 0
             }
